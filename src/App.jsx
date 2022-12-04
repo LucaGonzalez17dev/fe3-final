53328fe554b1c082b25e5cac5727205687304127
LucaGonzalez17dev <lucadxtx17@gmail.com>
2022-12-04 19:40:11 -0300

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
+import Contact from "./Routes/Contact";
+import Detail from "./Routes/Detail";
+import Favs from "./Routes/Favs";
+import Home from "./Routes/Home";
 
 
 function App() {
   return (
-      <div className="App">
-          <Navbar/>
-          <Footer/>
-      </div>
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+      <Route path="/home" element={<Home/>} />
+      <Route path="/contact" element={<Contact/>} />
+      <Route path="/dentist/:id" element={<Detail/>} />
+      <Route path="/favs" element={<Favs/>} />
+    </Routes>
+    <Footer/>
+</BrowserRouter>
   );
 }
 
